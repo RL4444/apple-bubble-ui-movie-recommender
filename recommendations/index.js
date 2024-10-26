761b307ab84418d26d107690387ec5b3756e3243
rl4444 <lewisroryjames@gmail.com>
2024-10-26 11:43:39 +0200

    [be] use only withPeople query param when building results

--- recommendations/index.js
+++ recommendations/index.js
@@ -8,17 +8,14 @@ const TMDB_API_KEY = process.env.TMDB_API_KEY;
 const fetchRecommendations = async (recommendationsDataRaw, currentPage = 1) => {
     console.log({ recommendationsDataRaw });
 
-    const directorsQueryParams = `&with_crew=${recommendationsDataRaw.directors.join("|")}`;
-    const actorsQueryParams = `&with_people=${recommendationsDataRaw.actors.join("|")}`;
+    const actorsQueryParams = `&with_people=${recommendationsDataRaw.directors.join("|")}|${recommendationsDataRaw.actors.join("|")}`;
     const genresQueryParams = `&with_genres=${recommendationsDataRaw.genres.join("|")}`;
 
     const url =
         "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=true&language=en-US&page=1&sort_by=popularity.desc" +
-        directorsQueryParams +
         actorsQueryParams +
         genresQueryParams;
 
-    console.log({ url });
     try {
         const response = await axios.get(`${url}`, {
             params: {
@@ -29,7 +26,7 @@ const fetchRecommendations = async (recommendationsDataRaw, currentPage = 1) => 
         });
 
         const { data } = response;
-        fs.writeFileSync("./recommendations/response.json", JSON.stringify({ data: data.results }));
+        // fs.writeFileSync("./recommendations/response.json", JSON.stringify({ data: data.results }));
         return { data: data.results, error: false, status: 200, message: null };
     } catch (error) {
         console.log("recommendations/index.js fetchRecommendations catch block -- ", { error });
